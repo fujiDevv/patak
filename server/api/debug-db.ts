675f95e7293a32879c47b583fd35f052cf46156c
Joshua Sarmiento <supremojoshua22@gmail.com>
2026-06-04 11:28:42 +0800

    Update debug-db endpoint to check INGEST_SECRET definition

--- server/api/debug-db.ts
+++ server/api/debug-db.ts
@@ -3,6 +3,8 @@ export default defineEventHandler((event) => {
     TURSO_DATABASE_URL_DEFINED: !!process.env.TURSO_DATABASE_URL,
     TURSO_DATABASE_URL_PREFIX: process.env.TURSO_DATABASE_URL ? process.env.TURSO_DATABASE_URL.substring(0, 15) : 'none',
     TURSO_AUTH_TOKEN_DEFINED: !!process.env.TURSO_AUTH_TOKEN,
+    INGEST_SECRET_DEFINED: !!process.env.INGEST_SECRET,
+    INGEST_SECRET_LENGTH: process.env.INGEST_SECRET ? process.env.INGEST_SECRET.length : 0,
     NODE_ENV: process.env.NODE_ENV
   };
 });
